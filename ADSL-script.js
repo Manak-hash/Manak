1aba79a71c1bd1ed183b724a37c5776ca60d2bcb
Manak <okhowazaki@gmail.com>
2024-12-18 11:47:07 +0100

    ADSL-script.js

--- ADSL-script.js
+++ ADSL-script.js
@@ -1,9 +1,9 @@
 const speedSlider = document.getElementById('speedSlider');
-const priceDisplay = document.getElementById('Mbps');
+const MbpsDisplay = document.getElementById('Mbps');
 
 const Mbps = [4, 8, 12, 20];
 
 speedSlider.addEventListener('input', function() {
     const selectedIndex = speedSlider.value;
-    priceDisplay.textContent = Mbps[selectedIndex];
+    MbpsDisplay.textContent = Mbps[selectedIndex];
 });
